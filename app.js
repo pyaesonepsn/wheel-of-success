195ee0717ba6b9795108937660c952c67b51447c
pyaesonepsn <pyaesonepsn@gmail.com>
2022-03-10 02:03:02 +0630

    edited

--- app.js
+++ app.js
@@ -1,57 +1,99 @@
-const phrases = [
-    "Demon Slayer",
-    "Pneumonia",
-    "HIV",
-    "Mandalay",
-    "Bangkok"
-]
 
+const phrases = [
+    "HIV",
+    "The Departed",
+    "The wolf of wallstreet",
+    "Titanic",
+    "TechDegree",
+]
 const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
-const startGame = document.getElementById('.btn-reset');
-const overlay = document.getElementById('overlay');
+const Phrase = document.getElementById('phrase');
+const startGame = document.querySelector('.btn__reset');
+const overlay =  document.getElementById("overlay")
 const title = document.querySelector('.title');
 const ul = document.querySelector('ul');
 let missed = 0;
 
-startGame.addEventListener('click', () => {
-    overlay.style.display: "none";
+startGame.addEventListener('click', () =>{
+    overlay.style.display = "none";
 });
-
-function getRandomPhraseAsArray (arr) {
-    let randomNumber = Math.floor(Math.random() * arr.length);
+function getRandomPhraseAsArray(arr) {
+    let randomNumber =  Math.floor(Math.random() * arr.length);
     let randomPhrase = arr[randomNumber].toLowerCase();
     let randomSplit = randomPhrase.split('');
     console.log(randomSplit);
     return randomSplit;
-}
-const phraseArray = getRandomPhraseAsArray(phrases);
+};
+const PhraseArray = getRandomPhraseAsArray(phrases);
 
 function addPhraseToDisplay (arr) {
-    for ( let i = 0; i < arr.length; i++) {
-        let list = document.createElement('li');
-        list.textContent = arr[i];
-        ul.appendChild(list);
-
-        if (arr[i] === ' ') {
-            list.className = 'space';
-        } else {
-            list.className = 'letter';
+    for (let i=0; i<arr.length; i++) {
+        let li = document.createElement('li');
+        li.textContent =arr[i];
+        ul.appendChild(li);
+        
+        if(arr[i]===' '){
+            li.className = 'space';
+        }else{
+            li.className = 'letter';
         }
     }
-}
-addPhraseToDisplay(phraseArray);
+};
+addPhraseToDisplay(PhraseArray);
 
 function checkLetter (button) {
     const listItems = document.querySelectorAll('li');
     let match = null;
-    for ( let i = 0; i < listItems.length; i++){
-        if (button.textContent === listItems[i].textContent.toLowerCase()){
+    for(let i=0; i<listItems.length; i++){
+        if(button.textContent === listItems[i].textContent.toLowerCase()){
             listItems[i].classList.add('show');
-            match = button.textContent ;
+            match = button.textContent;
         }
     }
-    return match;
-}
+        return match;
+};
 
-qwerty.addEventListener 
+qwerty.addEventListener ('click', (e) =>{
+    const btn = e.target;
+    if(btn.tagName === 'BUTTON' || btn.className === 'chosen'){
+        btn.className = 'chosen';
+        btn.disabled = true;
+        const letterFound = checkLetter(btn);
+        if (letterFound === null) {
+            const headLost = document.querySelectorAll('.tries img');
+            headLost[missed].src = 'images/lostHeart.png';
+            headLost.className = ' lost';
+            missed++;
+            console.log(headLost);
+        }
+    }
+        checkWin();
+});
+
+function checkWin() {
+    const letter = document.querySelectorAll('.letter');
+    const show = document.querySelectorAll('.show');
+    if (letter.length === show.length) {
+        overlay.className = 'win'; 
+        title.textContent = 'Your Win';
+        overlay.style.display = 'flex';
+    } else if (missed > 4 ) {
+        overlay.className = 'lose';
+        title.textContent = 'Your Lose';
+        overlay.style.display = 'flex';
+    }
+    
+};
+ 
+
+
+
+
+
+
+
+
+
+
+
+
